f33eff0afeec915b3bf6e49b1192150beeb3f849
MMEEUR <119952739+MMEEUR@users.noreply.github.com>
2023-01-19 17:32:55 +0000

    Set Default Media Root

--- MVT/Elearn/Elearn/settings.py
+++ MVT/Elearn/Elearn/settings.py
@@ -15,7 +15,6 @@ import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
 MEDIA_ROOT = os.path.join(BASE_DIR, 'uploads')
 
 # Quick-start development settings - unsuitable for production

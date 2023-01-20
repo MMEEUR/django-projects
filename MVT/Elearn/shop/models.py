29d2d2d52a0bf594ac25f314a5c8c17e34af7901
MMEEUR <119952739+MMEEUR@users.noreply.github.com>
2023-01-20 11:42:37 +0000

    Create Product & Category Models

--- MVT/Elearn/shop/models.py
+++ MVT/Elearn/shop/models.py
@@ -1,3 +1,30 @@
 from django.db import models
 
 # Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=200, db_index=True)
+    slug = models.SlugField(max_length=200, unique=True)
+    
+    class Meta:
+        ordering = ('name',)
+        
+    def __str__(self):
+        return self.name
+    
+class Product(models.Model):
+    category = models.ForeignKey(Category, related_name='products', on_delete=models.CASCADE)
+    name = models.CharField(max_length=100, db_index=True)
+    slug = models.CharField(max_length=100, db_index=True)
+    image = models.ImageField(upload_to='products/%Y/%m', blank=True)
+    description = models.TextField(blank=True)
+    price = models.DecimalField(max_digits=15, decimal_places=0)
+    available = models.BooleanField(default=True)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    
+    class Meta:
+        ordering = ('name',)
+        index_together = (('id'), ('slug'),)
+        
+    def __str__(self):
+        return self.name

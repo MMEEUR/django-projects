65b14e43c543bf7ce2c9666fce9b01bc1279a3f6
MMEEUR <119952739+MMEEUR@users.noreply.github.com>
2023-01-22 09:09:41 +0000

    Define Views For Shop

--- MVT/Elearn/shop/views.py
+++ MVT/Elearn/shop/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, get_object_or_404
+from .models import Category, Product
 # Create your views here.
+
+def productListView(request, cat_slug=None):
+    category = None
+    categories = Category.objects.all()
+    products = Product.objects.filter(available=True)
+    
+    if cat_slug:
+        category = get_object_or_404(Category, slug=cat_slug)
+        products = products.filter(category=category)
+        
+    return render(request, 'shop/product_list.html', {'category': category, 'categories': categories, 'products': products})
+
+def productDetailView(request, id, slug):
+    product = get_object_or_404(Product, id=id, slug=slug, available=True)
+    
+    return render(request, 'shop/product_detail.html', {'product': product})

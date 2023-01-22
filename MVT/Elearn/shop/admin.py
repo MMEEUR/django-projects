7650ede579c86e9c10b6a756422539e46ed28483
MMEEUR <119952739+MMEEUR@users.noreply.github.com>
2023-01-22 08:23:53 +0000

    Add Product & Category Models To Admin.py

--- MVT/Elearn/shop/admin.py
+++ MVT/Elearn/shop/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import Product, Category
 
 # Register your models here.
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('name', 'category', 'price', 'available')
+    list_filter = ('available', 'created')
+    ordering = ('available', 'created')
+    raw_id_fields = ('category',)
+    search_field = ('name',)
+    prepopulated_fields = {'slug':('name',)}
+    
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name',)

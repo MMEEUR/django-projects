7087a09c2d12e1c2f4d54088457d13b5f1385151
MMEEUR <119952739+MMEEUR@users.noreply.github.com>
2023-01-24 15:59:06 +0000

    Add Paginator To productListView

--- MVT/Elearn/shop/views.py
+++ MVT/Elearn/shop/views.py
@@ -1,17 +1,29 @@
 from django.shortcuts import render, get_object_or_404
+from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
 from .models import Category, Product
 # Create your views here.
 
 def productListView(request, cat_slug=None):
     category = None
     categories = Category.objects.all()
-    products = Product.objects.filter(available=True)
+    object_list = Product.objects.filter(available=True)
+    total_products = Product.objects.count()
     
     if cat_slug:
         category = get_object_or_404(Category, slug=cat_slug)
-        products = products.filter(category=category)
+        object_list = object_list.filter(category=category)
         
-    return render(request, 'shop/product_list.html', {'category': category, 'categories': categories, 'products': products})
+    paginator = Paginator(object_list, 4)
+    page = request.GET.get('page')
+    
+    try:
+        products = paginator.page(page)
+    except PageNotAnInteger:
+        products = paginator.page(1)
+    except EmptyPage:
+        products = paginator.page(paginator.num_pages)
+        
+    return render(request, 'shop/product_list.html', {'category': category, 'categories': categories, 'products': products, 'total_products': total_products})
 
 def productDetailView(request, id, slug):
     product = get_object_or_404(Product, id=id, slug=slug, available=True)

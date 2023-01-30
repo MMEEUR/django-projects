897092d5fb91b45f386442d47b5fd8d7594fa8af
MMEEUR <119952739+MMEEUR@users.noreply.github.com>
2023-01-30 18:08:00 +0000

    Set url for cart app

--- MVT/Elearn/Elearn/urls.py
+++ MVT/Elearn/Elearn/urls.py
@@ -20,5 +20,6 @@ from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('shop/', include('shop.urls', namespace='shop'))
+    path('shop/', include('shop.urls', namespace='shop')),
+    path('cart/', include('cart.urls', namespace='cart')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)

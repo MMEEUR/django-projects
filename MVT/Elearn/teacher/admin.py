669b573de0445da36a6f6d732e48be30ada46171
MMEEUR <119952739+MMEEUR@users.noreply.github.com>
2023-02-01 15:38:10 +0000

    Add Teacher Admin

--- MVT/Elearn/teacher/admin.py
+++ MVT/Elearn/teacher/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Teacher
 
 # Register your models here.
+@admin.register(Teacher)
+class TeacherAdmin(admin.ModelAdmin):
+    list_display = ('name', 'role')
+    list_filter = ('role',)
+    search_field = ('name',)

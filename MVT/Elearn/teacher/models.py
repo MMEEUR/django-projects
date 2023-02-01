e51db46b8d09bf80763b1054864425a70f30eedb
MMEEUR <119952739+MMEEUR@users.noreply.github.com>
2023-02-01 15:37:44 +0000

    Add Teacher Models

--- MVT/Elearn/teacher/models.py
+++ MVT/Elearn/teacher/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+
+class Teacher(models.Model):
+    name = models.TextField(max_length=50)
+    slug = models.TextField(max_length=50)
+    biography = models.CharField(max_length=150)
+    about = models.TextField(max_length=250)
+    about = models.TextField(max_length=250)
+    achievements = models.TextField(max_length=250)
+    objectives = models.TextField(max_length=250)
+    role = models.CharField(max_length=20)
+    
+    class Meta:
+        ordering = ('name',)
+        
+    def __str__(self):
+        return self.name

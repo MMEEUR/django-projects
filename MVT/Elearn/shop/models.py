49a7acca528e254fa12da4121edbb3f34079c467
MMEEUR <119952739+MMEEUR@users.noreply.github.com>
2023-02-01 15:40:56 +0000

    Change the product model and add Lecture model

--- MVT/Elearn/shop/models.py
+++ MVT/Elearn/shop/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.urls import reverse
+from teacher.models import Teacher
 
 # Create your models here.
 class Category(models.Model):
@@ -15,16 +16,19 @@ class Category(models.Model):
     def __str__(self):
         return self.name
     
+    
 class Product(models.Model):
+    teacher = models.ForeignKey(Teacher, on_delete=models.CASCADE, related_name='courses')
     category = models.ForeignKey(Category, related_name='products', on_delete=models.CASCADE)
     name = models.CharField(max_length=100, db_index=True)
     slug = models.CharField(max_length=100, db_index=True)
     image = models.ImageField(upload_to='products/%Y/%m', blank=True)
-    image2 = models.ImageField(upload_to='products/%Y/%m', blank=True)
-    image3 = models.ImageField(upload_to='products/%Y/%m', blank=True)
-    image4 = models.ImageField(upload_to='products/%Y/%m', blank=True)
-    description = models.TextField(blank=True)
-    price = models.DecimalField(max_digits=15, decimal_places=0)
+    summery = models.CharField(max_length=250)
+    requirements = models.CharField(max_length=250)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    duaration = models.DurationField()
+    quizzes = models.IntegerField()
+    students = models.IntegerField(default=0)
     available = models.BooleanField(default=True)
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
@@ -38,3 +42,15 @@ class Product(models.Model):
     
     def __str__(self):
         return self.name
+    
+class Lecture(models.Model):
+    name = models.CharField(max_length=50)
+    description = models.TextField()
+    duaration = models.DurationField()
+    course = models.ForeignKey(Product, related_name='lectures', on_delete=models.CASCADE)
+    
+    class Meta:
+        ordering = ('course','id')
+    
+    def __str__(self):
+        return f'{self.course}-{self.id}-{self.name}'

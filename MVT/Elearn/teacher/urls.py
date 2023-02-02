90e784adeee19da39eab5296cc3ddbe29dc06a8e
MMEEUR <119952739+MMEEUR@users.noreply.github.com>
2023-02-02 16:05:41 +0000

    Set url for teacher list and single page

--- MVT/Elearn/teacher/urls.py
+++ MVT/Elearn/teacher/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
+from .views import teacherListView, teacherDetailView
 
 app_name = 'teacher'
 
 urlpatterns = [
-    
+    path('', teacherListView, name='teacher_list'),
+    path('<slug:slug>', teacherDetailView, name='teacher_detail')
 ]

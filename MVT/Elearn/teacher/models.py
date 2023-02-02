0f597b12e713bf8b06460ee51f9e054609d074dc
MMEEUR <119952739+MMEEUR@users.noreply.github.com>
2023-02-02 15:24:51 +0000

    Add Picture Field

--- MVT/Elearn/teacher/models.py
+++ MVT/Elearn/teacher/models.py
@@ -3,13 +3,13 @@ from django.db import models
 # Create your models here.
 
 class Teacher(models.Model):
-    name = models.TextField(max_length=50)
-    slug = models.TextField(max_length=50)
+    name = models.CharField(max_length=50)
+    slug = models.CharField(max_length=50)
+    picture = models.ImageField(upload_to='teachers/%Y/%m', null=True)
     biography = models.CharField(max_length=150)
-    about = models.TextField(max_length=250)
-    about = models.TextField(max_length=250)
-    achievements = models.TextField(max_length=250)
-    objectives = models.TextField(max_length=250)
+    about = models.CharField(max_length=250)
+    achievements = models.CharField(max_length=250)
+    objectives = models.CharField(max_length=250)
     role = models.CharField(max_length=20)
     
     class Meta:

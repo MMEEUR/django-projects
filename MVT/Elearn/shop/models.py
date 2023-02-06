80342df363f4566d7115441d2b3d1a605e3ab83e
MMEEUR <119952739+MMEEUR@users.noreply.github.com>
2023-02-06 14:24:17 +0000

    Add Review model

--- MVT/Elearn/shop/models.py
+++ MVT/Elearn/shop/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.urls import reverse
+from django.core.validators import MinValueValidator, MaxValueValidator
 from teacher.models import Teacher
 
 # Create your models here.
@@ -18,16 +19,16 @@ class Category(models.Model):
     
     
 class Product(models.Model):
-    teacher = models.ForeignKey(Teacher, on_delete=models.CASCADE, related_name='courses')
+    teacher = models.ForeignKey(Teacher, on_delete=models.CASCADE, related_name='courses', null=True)
     category = models.ForeignKey(Category, related_name='products', on_delete=models.CASCADE)
     name = models.CharField(max_length=100, db_index=True)
     slug = models.CharField(max_length=100, db_index=True)
     image = models.ImageField(upload_to='products/%Y/%m', blank=True)
-    summery = models.CharField(max_length=250)
-    requirements = models.CharField(max_length=250)
+    summery = models.CharField(max_length=250, null=True)
+    requirements = models.CharField(max_length=250, null=True)
     price = models.DecimalField(max_digits=10, decimal_places=2)
-    duaration = models.DurationField()
-    quizzes = models.IntegerField()
+    duaration = models.DurationField(null=True)
+    quizzes = models.IntegerField(null=True)
     students = models.IntegerField(default=0)
     available = models.BooleanField(default=True)
     created = models.DateTimeField(auto_now_add=True)
@@ -39,6 +40,13 @@ class Product(models.Model):
         
     def get_absolute_url(self):
         return reverse("shop:product_detail", args=[self.id, self.slug])
+    
+    def average_rating(self):
+        rating = []
+        for i in self.reviews.filter(rating__isnull=False, active=True):
+            rating.append(getattr(i, 'rating'))
+            
+        return sum(rating) / len(rating) if rating else 0
     
     def __str__(self):
         return self.name
@@ -54,3 +62,12 @@ class Lecture(models.Model):
     
     def __str__(self):
         return f'{self.course}-{self.id}-{self.name}'
+    
+class Review(models.Model):
+    course = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='reviews')
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    rating = models.IntegerField(null=True, validators=[MinValueValidator(1), MaxValueValidator(5)])
+    comment = models.TextField()
+    created = models.DateField(auto_now_add=True)
+    active = models.BooleanField(default=False)

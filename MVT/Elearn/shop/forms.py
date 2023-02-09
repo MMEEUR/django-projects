799532f0fa9dea0dde1415ab23c2c8c0c006124f
MMEEUR <119952739+MMEEUR@users.noreply.github.com>
2023-02-09 18:43:42 +0000

    Change comment form

--- MVT/Elearn/shop/forms.py
+++ MVT/Elearn/shop/forms.py
@@ -4,4 +4,15 @@ from .models import Review
 class ReviewForm(forms.ModelForm):
     class Meta:
         model = Review
-        fields = ('first_name', 'last_name', 'rating', 'comment')
+        fields = ['first_name', 'last_name', 'comment', 'rating', 'course']
+        labels = {
+            'rating': '',
+            'course': '',
+        }
+        widgets = {
+            'rating': forms.HiddenInput(),
+            'course': forms.HiddenInput(),
+            'first_name': forms.TextInput(attrs={'placeholder': 'First Name'}),
+            'last_name': forms.TextInput(attrs={'placeholder': 'Last Name'}),
+            'comment': forms.Textarea(attrs={'placeholder': 'Comment'}),
+        }

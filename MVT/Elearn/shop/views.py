8e7aa8861e1ae5cdf81b2fff2f892a6eb4a6ab56
MMEEUR <119952739+MMEEUR@users.noreply.github.com>
2023-02-09 18:40:27 +0000

    Add comment system

--- MVT/Elearn/shop/views.py
+++ MVT/Elearn/shop/views.py
@@ -1,17 +1,18 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
+from django.views.decorators.csrf import csrf_exempt
 from .models import Category, Product
+from .forms import ReviewForm
 # Create your views here.
 
-def productListView(request, cat_slug=None):
+def product_list_view(request, cat_slug=None):
     category = None
-    categories = Category.objects.all()
     object_list = Product.objects.filter(available=True)
     
     if cat_slug:
         category = get_object_or_404(Category, slug=cat_slug)
         object_list = object_list.filter(category=category)
-    
+        
     products_count = object_list.count()
     paginator = Paginator(object_list, 6)
     page = request.GET.get('page')
@@ -23,12 +24,22 @@ def productListView(request, cat_slug=None):
     except EmptyPage:
         products = paginator.page(paginator.num_pages)
         
-    return render(request, 'shop/product_list.html', {'category': category, 'categories': categories, 'products': products, 'products_count': products_count})
+    return render(request, 'shop/product_list.html', {'category': category, 'products': products, 'products_count': products_count})
 
-def productDetailView(request, id, slug):
+def product_detail_view(request, id, slug):
     product = get_object_or_404(Product, id=id, slug=slug, available=True)
     product_lectures = product.lectures.all()
+    product_reviews = product.reviews.filter(active=True).order_by('-created')
     latest_products = Product.objects.filter(available=True).exclude(id=product.id)[:3]
     releted_products = Product.objects.filter(category=product.category, available=True).exclude(id=product.id)
     
-    return render(request, 'shop/product_detail.html', {'product': product, 'releted_products': releted_products, 'product_lectures': product_lectures, 'latest_products': latest_products})
+    if request.method == 'POST':
+        form = ReviewForm(request.POST)
+        if form.is_valid():
+            form.save()
+            
+            return redirect(product.get_absolute_url())
+    else:
+        form = ReviewForm(initial={'course': product})
+    
+    return render(request, 'shop/product_detail.html', {'product': product, 'form': form, 'product_reviews': product_reviews, 'releted_products': releted_products, 'product_lectures': product_lectures, 'latest_products': latest_products})

b8d764103bf0a61f8a45543a43ba2682cbea5bdc
MMEEUR <119952739+MMEEUR@users.noreply.github.com>
2023-02-09 18:40:53 +0000

    Update views names

--- MVT/Elearn/shop/urls.py
+++ MVT/Elearn/shop/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path, register_converter
 from django.urls.converters import SlugConverter
-from .views import productDetailView, productListView
+from .views import product_detail_view, product_list_view
 
 app_name = 'shop'
 
@@ -10,7 +10,7 @@ class PersianSlugConvertor(SlugConverter):
 register_converter(PersianSlugConvertor, 'persian_slug')
 
 urlpatterns = [
-    path('', productListView, name='product_list'),
-    path('<persian_slug:category_slug', productListView, name='product_list_by_category'),
-    path('<int:id>/<persian_slug:slug>', productDetailView, name='product_detail'),
+    path('', product_list_view, name='product_list'),
+    path('<persian_slug:cat_slug>', product_list_view, name='product_list_by_category'),
+    path('<int:id>/<persian_slug:slug>', product_detail_view, name='product_detail'),
 ]

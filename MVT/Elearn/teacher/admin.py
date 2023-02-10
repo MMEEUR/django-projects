e8d3534c53be3fc6a2ac276c7b018dc3bfb5ec05
MMEEUR <119952739+MMEEUR@users.noreply.github.com>
2023-02-10 13:04:47 +0000

    Add Review to admin

--- MVT/Elearn/teacher/admin.py
+++ MVT/Elearn/teacher/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Teacher
+from .models import Teacher, Review
 
 # Register your models here.
 @admin.register(Teacher)
@@ -8,3 +8,10 @@ class TeacherAdmin(admin.ModelAdmin):
     list_filter = ('role',)
     search_field = ('name',)
     prepopulated_fields = {'slug':('name',)}
+    
+@admin.register(Review)
+class ReviewAdmin(admin.ModelAdmin):
+    list_display = ('first_name', 'last_name', 'rating', 'created', 'active')
+    list_filter = ('active',)
+    ordering = ('active', '-created')
+    search_field = ('first_name', 'last_name', 'comment')

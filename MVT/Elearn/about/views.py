5821f132c1c6bb2568db5e1e702cb8d6110459bb
MMEEUR <119952739+MMEEUR@users.noreply.github.com>
2023-02-11 12:25:35 +0000

    Create view for about_us page

--- MVT/Elearn/about/views.py
+++ MVT/Elearn/about/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
+from shop.models import Product
+from teacher.models import Teacher
 
 # Create your views here.
+def about_us(request):
+    teachers = Teacher.objects.all()
+    total_students = 0
+    total_courses = Product.objects.all().count()
+    
+    for i in Product.objects.all():
+        total_students += i.students
+        
+    context = {
+        'teachers': teachers,
+        'total_students': total_students,
+        'total_courses': total_courses
+    }
+        
+    return render(request, 'about/about_us.html', context=context)

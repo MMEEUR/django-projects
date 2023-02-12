24a03a21525728093543151dc9142ecec4300315
MMEEUR <119952739+MMEEUR@users.noreply.github.com>
2023-02-12 13:40:02 +0000

    Add Blog models to admin

--- MVT/Elearn/blog/admin.py
+++ MVT/Elearn/blog/admin.py
@@ -1,3 +1,33 @@
 from django.contrib import admin
+from .models import Post, Category, Comment, Reply
 
-# Register your models here.
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'category', 'author', 'created_at', 'updated_at', 'active')
+    list_filter = ('active', 'category')
+    ordering = ('active', '-created_at')
+    raw_id_fields = ('category',)
+    search_fields = ('title', 'content', 'author')
+    
+    def save_model(self, request, obj, form, change):
+        obj.author = request.user
+        super().save_model(request, obj, form, change)
+    
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name', 'slug')
+    ordering = ('name',)
+    
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('author', 'post', 'created_at', 'active')
+    list_filter = ('active',)
+    ordering = ('active', '-created_at')
+    search_fields = ('author', 'text', 'post')
+    
+@admin.register(Reply)
+class ReplyAdmin(admin.ModelAdmin):
+    list_display = ('author', 'comment', 'created_at', 'active')
+    list_filter = ('active',)
+    ordering = ('active', '-created_at')
+    search_fields = ('author', 'text', 'comment')

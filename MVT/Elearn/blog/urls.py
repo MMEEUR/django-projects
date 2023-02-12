69ad2d61b2be1a0dd90870a1be4805c960b1491d
MMEEUR <119952739+MMEEUR@users.noreply.github.com>
2023-02-12 17:03:03 +0000

    Set url for Blog

--- MVT/Elearn/blog/urls.py
+++ MVT/Elearn/blog/urls.py
@@ -1,7 +1,10 @@
-from django import urls
+from django.urls import path
+from .views import post_list_view, post_detail_view
 
 app_name = 'blog'
 
 urlpatterns = [
-    
+    path('', post_list_view, name='post_list'),
+    path('<int:post_id>', post_detail_view, name='post_detail'),
+    path('<slug:cat_slug>', post_list_view, name='post_list_by_cat'),
 ]

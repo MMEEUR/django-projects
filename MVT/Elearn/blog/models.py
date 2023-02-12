569e7daeb6c6f93161c650dac88b66b56c197f7c
MMEEUR <119952739+MMEEUR@users.noreply.github.com>
2023-02-12 17:03:43 +0000

    Create comments_count for Post

--- MVT/Elearn/blog/models.py
+++ MVT/Elearn/blog/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.urls import reverse
 from django.contrib.auth.models import User
 from django.utils.text import slugify
 
@@ -15,6 +16,9 @@ class Category(models.Model):
     def save(self, *args, **kwargs):
         self.slug = slugify(self.name)
         super().save(*args, **kwargs)
+        
+    def get_absolute_url(self):
+        return reverse("blog:post_list_by_cat", args=[self.slug])
 
 class Post(models.Model):
     title = models.CharField(max_length=100)
@@ -27,10 +31,16 @@ class Post(models.Model):
     active = models.BooleanField(default=True)
     
     class Meta:
-        ordering = ('name',)
+        ordering = ('title',)
     
     def __str__(self):
         return self.title
+    
+    def comments_count(self):
+        return self.comments.count()
+    
+    def get_absolute_url(self):
+        return reverse("blog:post_detail", args=[self.id])
 
 class Comment(models.Model):
     post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')

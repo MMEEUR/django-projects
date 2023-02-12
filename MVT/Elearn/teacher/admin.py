8c7be7b427d440cca300d8a5a68e9d9781e5f75a
MMEEUR <119952739+MMEEUR@users.noreply.github.com>
2023-02-12 13:42:12 +0000

    Add search_fields to admin models

--- MVT/Elearn/teacher/admin.py
+++ MVT/Elearn/teacher/admin.py
@@ -6,7 +6,7 @@ from .models import Teacher, Review
 class TeacherAdmin(admin.ModelAdmin):
     list_display = ('name', 'role')
     list_filter = ('role',)
-    search_field = ('name',)
+    search_fields = ('name',)
     prepopulated_fields = {'slug':('name',)}
     
 @admin.register(Review)
@@ -14,4 +14,4 @@ class ReviewAdmin(admin.ModelAdmin):
     list_display = ('first_name', 'last_name', 'rating', 'created', 'active')
     list_filter = ('active',)
     ordering = ('active', '-created')
-    search_field = ('first_name', 'last_name', 'comment')
+    search_fields = ('first_name', 'last_name', 'comment')

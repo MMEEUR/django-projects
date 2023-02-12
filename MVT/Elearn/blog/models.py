f61c27214cf44e7d5a98f94c9949526428014094
MMEEUR <119952739+MMEEUR@users.noreply.github.com>
2023-02-12 13:37:36 +0000

    Create Reply model

--- MVT/Elearn/blog/models.py
+++ MVT/Elearn/blog/models.py
@@ -5,6 +5,9 @@ from django.utils.text import slugify
 class Category(models.Model):
     name = models.CharField(max_length=100, unique=True)
     slug = models.SlugField(max_length=100, unique=True, editable=False)
+    
+    class Meta:
+        ordering = ('name',)
 
     def __str__(self):
         return self.name
@@ -23,17 +26,36 @@ class Post(models.Model):
     author = models.ForeignKey(User, on_delete=models.CASCADE, editable=False)
     active = models.BooleanField(default=True)
     
+    class Meta:
+        ordering = ('name',)
+    
     def __str__(self):
         return self.title
 
 class Comment(models.Model):
     post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
-    parent = models.ForeignKey('self', null=True, blank=True, on_delete=models.CASCADE, related_name='replies')
     author = models.CharField(max_length=100)
     text = models.TextField()
     email = models.EmailField()
     created_at = models.DateTimeField(auto_now_add=True)
     active = models.BooleanField(default=False)
+    
+    class Meta:
+        ordering = ('-created_at',)
+    
+    def __str__(self):
+        return self.text
+    
+class Reply(models.Model):
+    comment = models.ForeignKey(Comment, on_delete=models.CASCADE, related_name='replies')
+    author = models.CharField(max_length=100)
+    text = models.TextField()
+    email = models.EmailField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    active = models.BooleanField(default=False)
+    
+    class Meta:
+        ordering = ('-created_at',)
     
     def __str__(self):
         return self.text

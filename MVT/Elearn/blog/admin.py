fa5bfd8acc8ad9c4bfcb9e908ba22b3c5f780b7d
MMEEUR <119952739+MMEEUR@users.noreply.github.com>
2023-02-14 15:04:11 +0000

    Add parent to CommentAdmin

--- MVT/Elearn/blog/admin.py
+++ MVT/Elearn/blog/admin.py
@@ -1,9 +1,9 @@
 from django.contrib import admin
-from .models import Post, Category, Comment, Reply
+from .models import Post, Category, Comment
 
 @admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
-    list_display = ('title', 'category', 'author', 'created_at', 'updated_at', 'active')
+    list_display = ('id', 'title', 'category', 'author', 'created_at', 'updated_at', 'active')
     list_filter = ('active', 'category')
     ordering = ('active', '-created_at')
     raw_id_fields = ('category',)
@@ -20,14 +20,7 @@ class CategoryAdmin(admin.ModelAdmin):
     
 @admin.register(Comment)
 class CommentAdmin(admin.ModelAdmin):
-    list_display = ('author', 'post', 'created_at', 'active')
+    list_display = ('id', 'author', 'post', 'parent', 'created_at', 'active')
     list_filter = ('active',)
-    ordering = ('active', '-created_at')
-    search_fields = ('author', 'text', 'post')
-    
-@admin.register(Reply)
-class ReplyAdmin(admin.ModelAdmin):
-    list_display = ('author', 'comment', 'created_at', 'active')
-    list_filter = ('active',)
-    ordering = ('active', '-created_at')
-    search_fields = ('author', 'text', 'comment')
+    ordering = ('active', 'parent', '-created_at')
+    search_fields = ('author', 'text', 'post', 'parent')

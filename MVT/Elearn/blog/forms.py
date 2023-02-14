d0c41c35f7f92204a0e1392c8fecdfe90cf52efd
MMEEUR <119952739+MMEEUR@users.noreply.github.com>
2023-02-14 15:04:52 +0000

    Delete unnecessary Form

--- MVT/Elearn/blog/forms.py
+++ MVT/Elearn/blog/forms.py
@@ -1,22 +1,11 @@
 from django import forms
-from .models import Comment, Reply
+from .models import Comment
 
 class CommentForm(forms.ModelForm):
     class Meta:
         model = Comment
         fields = ('author', 'text', 'email')
         widgets = {
-            'author': forms.TextInput(attrs={'placeholder': 'Name', 'type': 'text'}),
-            'email': forms.TextInput(attrs={'placeholder': 'Email', 'type': 'email'}),
-            'text': forms.Textarea(attrs={'placeholder': 'Comment'}),
-        }
-
-class ReplyForm(forms.ModelForm):
-    class Meta:
-        model = Reply
-        fields = ('comment', 'author', 'text', 'email')
-        widgets = {
-            'comment': forms.HiddenInput(),
             'author': forms.TextInput(attrs={'placeholder': 'Name', 'type': 'text'}),
             'email': forms.TextInput(attrs={'placeholder': 'Email', 'type': 'email'}),
             'text': forms.Textarea(attrs={'placeholder': 'Comment'}),

a757be3dcd9ed75f1a1b3b8492608973c3418396
MMEEUR <119952739+MMEEUR@users.noreply.github.com>
2023-02-15 17:11:22 +0000

    Add Event model to admin

--- MVT/Elearn/events/admin.py
+++ MVT/Elearn/events/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Event
 
 # Register your models here.
+@admin.register(Event)
+class EventAdmin(admin.ModelAdmin):
+    list_display = ('title', 'event_day', 'start_time', 'end_time', 'place', 'address', 'active')
+    list_filter = ('active',)
+    ordering = ('event_day', 'start_time', 'end_time', 'active')
+    search_fields = ('title', 'content', 'place', 'address')

1a80b1c5b9943464f6efee9939f89aa7e47bc9e4
MMEEUR <119952739+MMEEUR@users.noreply.github.com>
2023-02-18 14:16:29 +0000

    Set url for Cart views

--- MVT/Elearn/cart/urls.py
+++ MVT/Elearn/cart/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
+from .views import cart_detail, add_to_cart, remove_from_cart
 
 app_name = 'cart'
 
 urlpatterns = [
-    path(),
+   path('', cart_detail, name='cart_detail'),
+   path('add/<int:product_id>', add_to_cart, name='cart_add'),
+   path('remove/<int:product_id>', remove_from_cart, name='cart_remove'),
 ]

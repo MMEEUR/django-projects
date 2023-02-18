41cc13c9cd4c93bd253648d9116d9ad816bf6825
MMEEUR <119952739+MMEEUR@users.noreply.github.com>
2023-02-18 14:16:00 +0000

    Create Cart views

--- MVT/Elearn/cart/views.py
+++ MVT/Elearn/cart/views.py
@@ -1,3 +1,31 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
+from django.views.decorators.http import require_POST
+from shop.models import Product
+from .forms import CartAddForm
+from .cart import Cart
 
 # Create your views here.
+@require_POST
+def add_to_cart(request, product_id):
+    product = get_object_or_404(Product, id=product_id)
+    cart = Cart(request)
+    cart_form = CartAddForm(request.POST)
+    
+    if cart_form.is_valid():
+        cd = cart_form.cleaned_data
+        cart.add(product=product, quantity=cd['quantity'], override_quantity=cd['override_quantity'])
+        
+    return redirect("cart:cart_detail")
+
+@require_POST
+def remove_from_cart(request, product_id):
+    product = get_object_or_404(Product, id=product_id)
+    cart = Cart(request) 
+    cart.remove(product=product)
+    
+    return redirect("cart:cart_detail")
+
+def cart_detail(request):
+    cart = Cart(request)
+    
+    return render(request, 'cart/cart_detail.html', {'cart': cart})

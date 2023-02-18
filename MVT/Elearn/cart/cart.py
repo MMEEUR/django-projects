94d0aafc5adbbd53daf300db1e59f939e59940e2
MMEEUR <119952739+MMEEUR@users.noreply.github.com>
2023-02-18 14:15:14 +0000

    Change function name

--- MVT/Elearn/cart/cart.py
+++ MVT/Elearn/cart/cart.py
@@ -25,7 +25,7 @@ class Cart(object):
     def __len__(self):
         return sum(item['quantity'] for item in self.cart.values())
         
-    def total_price(self):
+    def get_total_price(self):
         return sum(Decimal(item['price']) * item['quantity']  for item in self.cart.values())
         
     def add(self, product, quantity=1, override_quantity=False):

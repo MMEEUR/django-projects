f8e9537a7c8a71f53cea8386785775e52ea278b0
MMEEUR <119952739+MMEEUR@users.noreply.github.com>
2023-02-20 16:27:55 +0000

    Add coerce to quantity

--- MVT/Elearn/cart/forms.py
+++ MVT/Elearn/cart/forms.py
@@ -3,5 +3,5 @@ from django import forms
 CHOISES = [(i, str(i)) for i in range(1, 21)]
 
 class CartAddForm(forms.Form):
-    quantity = forms.TypedChoiceField(choices=CHOISES)
+    quantity = forms.TypedChoiceField(choices=CHOISES, coerce=int)
     override = forms.BooleanField(required=False, initial=False, widget=forms.HiddenInput)

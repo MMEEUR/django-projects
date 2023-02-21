0befc883f9fa8fab1f90a2631a0b676fea52818b
MMEEUR <119952739+MMEEUR@users.noreply.github.com>
2023-02-21 16:31:09 +0000

    Add Order models to admin

--- MVT/Elearn/orders/admin.py
+++ MVT/Elearn/orders/admin.py
@@ -1,1 +1,13 @@
 from django.contrib import admin
+from .models import Order, OrderItem
+
+class OrderItemInline(admin.TabularInline):
+    model = OrderItem
+    raw_id_fields = ('product',)
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ('first_name', 'last_name', 'email', 'address', 'postal_code', 'city', 'created', 'updated', 'has_paid')
+    list_filter = ('has_paid', 'created', 'updated')
+    search_fields = ('first_name', 'last_name', 'city')
+    inlines = (OrderItemInline,)

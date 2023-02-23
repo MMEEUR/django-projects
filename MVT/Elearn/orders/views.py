132d9828d4c1983fb13c6a83a6e285287021aa94
MMEEUR <119952739+MMEEUR@users.noreply.github.com>
2023-02-23 10:00:11 +0000

    Create order_create view

--- MVT/Elearn/orders/views.py
+++ MVT/Elearn/orders/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
+from cart.cart import Cart
+from .forms import OrderCreateForm
+from .models import OrderItem
 
 # Create your views here.
+def order_create(request):
+    cart = Cart(request)
+    
+    if request.method == 'POST':
+        order_form = OrderCreateForm(data=request.POST)
+        if order_form.is_valid():
+            order = order_form.save()
+            for item in cart:
+                OrderItem.objects.create(order=order, product=item['product'], price=item['price'], quantity=item['quantity'])
+            cart.clear()
+            
+            return render(request, 'orders/order_created.html', {'order': order})
+    else:
+        order_form = OrderCreateForm()
+        
+    return render(request, 'orders/order_create.html', {'cart': cart, 'order_form': order_form})

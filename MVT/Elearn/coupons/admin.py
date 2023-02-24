994dcb1696eef026e9aea095469a206d26cf39d1
MMEEUR <119952739+MMEEUR@users.noreply.github.com>
2023-02-24 18:16:00 +0000

    Add Coupon to admin

--- MVT/Elearn/coupons/admin.py
+++ MVT/Elearn/coupons/admin.py
@@ -1,3 +1,35 @@
 from django.contrib import admin
+from .models import Coupon
+from django.utils.timesince import timesince
+from django.utils import timezone
 
 # Register your models here.
+@admin.register(Coupon)
+class CouponAdmin(admin.ModelAdmin):
+    list_display = ['code', 'time_until_expired', 'starts', 'ends', 'duration', 'active']
+    list_filter = ['active', 'starts', 'ends']
+    search_fields = ['code',]
+    ordering = ['starts', 'ends']
+    
+    def duration(self, obj):
+        delta = obj.ends - obj.starts
+        return delta
+    
+    def time_until_expired(self, obj):
+        if obj.active:           
+            now = timezone.now()
+            if obj.ends <= now:
+                return 'Expired'
+            else:
+                delta = obj.ends - now
+                days = delta.days
+                hours = delta.seconds // 3600
+                minutes = (delta.seconds // 60) % 60
+                if days > 0:
+                    return f"{days}d {hours}h {minutes}m"
+                elif hours > 0:
+                    return f"{hours}h {minutes}m"
+                else:
+                    return f"{minutes}m"
+        else:
+            return 'Coupon is not active'

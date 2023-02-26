aaaed6ef90f51d1514f21005f549e27bdcdfc998
MMEEUR <119952739+MMEEUR@users.noreply.github.com>
2023-02-26 12:59:08 +0000

    Add order coupon & discount

--- MVT/Elearn/orders/views.py
+++ MVT/Elearn/orders/views.py
@@ -10,7 +10,11 @@ def order_create(request):
     if request.method == 'POST':
         order_form = OrderCreateForm(data=request.POST)
         if order_form.is_valid():
-            order = order_form.save()
+            order = order_form.save(commit=False)
+            if cart.coupon:
+                order.coupon = cart.coupon
+                order.discount = cart.coupon.discount
+            order.save()
             for item in cart:
                 OrderItem.objects.create(order=order, product=item['product'], price=item['price'], quantity=item['quantity'])
             cart.clear()
